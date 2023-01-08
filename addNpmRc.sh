283d5b3fe3effe0863c0220f562d1bee00f73620
Richie <ricsam@users.noreply.github.com>
2023-01-08 15:32:23 +0100

    fix: add name to generated package.json files add cjs

---
+++ addNpmRc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for file in ./packages/*; do
+
+echo $file
+
+cat $file/package.json | jsonc modify -n -p '["publishConfig"]' -v '{"registry":"$1"}' -f $file/package.json
+
+done
+
+
