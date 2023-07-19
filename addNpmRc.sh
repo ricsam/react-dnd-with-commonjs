d70cd5868278362ae8e8a8068b4785ca57a21432
Richie <ricsam@users.noreply.github.com>
2023-07-19 10:23:34 +0200

    fix: add forked tag in the package description to separate forked npm package from non-forked version

--- addNpmRc.sh
+++ addNpmRc.sh
@@ -4,7 +4,7 @@ for file in ./packages/*; do
 
 echo $file
 
-cat $file/package.json | jsonc modify -n -p '["publishConfig"]' -v '{"registry":"$1"}' -f $file/package.json
+cat $file/package.json | jsonc modify -n -p '["publishConfig"]' -v "{\"registry\":\"$1\"}" -f $file/package.json
 
 done
 
